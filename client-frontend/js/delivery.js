2ec38189349c74659fdba9970773583719ae8af8
OpenShift guest <feedmewithspam@web.de>
2013-10-27 09:25:28 -0400

    Added the geocoding request which shows the client position

--- client-frontend/js/delivery.js
+++ client-frontend/js/delivery.js
@@ -1,18 +1,20 @@
 (function() {
 
     var App = function() {
-        this.orderId = window.location.hash.replace('#','');
-        this.getOrderUrl = '../mocks/getAllOrders.json';
+        this.orderId = window.location.hash.replace('#', '');
+        this.getOrderUrl = '../mocks/getSingleOrder.json';
+        this.clientMarker;
     };
 
     App.prototype.initMap = function() {
         var mapOptions = {
             center: new google.maps.LatLng(-34.397, 150.644),
-            zoom: 8,
+            zoom: 16,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
+        this.map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
+        this.geocoder = new google.maps.Geocoder();
 
     };
 
@@ -22,15 +24,36 @@
 
         }).then(_.bind(this.updateOrderPosition, this));
     };
-    
-    App.prototype.updateOrderPosition = function () {
-        
+
+    App.prototype.updateOrderPosition = function(data) {
+        if (!this.clientMarker) {
+            this.setClientPosition(data.deliveryAddress);
+        }
+
     };
-    
-    
-    $(document).ready(function () {
+
+    App.prototype.setClientPosition = function(address) {
+        this.geocoder.geocode({
+            'address': address
+        }, _.bind(function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                this.map.setCenter(results[0].geometry.location);
+                this.clientMarker = new google.maps.Marker({
+                    map: this.map,
+                    position: results[0].geometry.location
+                });
+            }
+            else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        }, this));
+    };
+
+
+    $(document).ready(function() {
         var app = new App();
         app.initMap();
+        app.getOrderData();
     });
 
 }());
